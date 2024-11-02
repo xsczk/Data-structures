92d68088cfd119bb03b0fa3d88c466e0551ea95e
Nghia Le Minh <wowminhnghia@gmail.com>
2024-11-02 13:06:21 +0700

    implement push and pop method for doubly linked list

--- src/doublyLinkedList.js
+++ src/doublyLinkedList.js
@@ -12,4 +12,42 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (!this.length) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+    return currentTail;
+  }
 }
+
+const first = new DoublyLinkedList();
+first.push('Hi');
+first.push("I'm");
+first.push('John');
+first.pop();
+first.pop();
+first.pop();
+console.log(first);
