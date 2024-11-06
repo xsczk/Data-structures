ba87ded292883c80c7806dd2b9c3c11bd53d6512
Nghia Le Minh <wowminhnghia@gmail.com>
2024-11-07 00:13:17 +0700

    implement binary search tree and its insert method

---
+++ src/binarySearchTree.js
@@ -0,0 +1,59 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let currentNode = this.root;
+    while (true) {
+      if (currentNode.val > val) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return this;
+        }
+        currentNode = currentNode.left;
+      } else if (currentNode.val < val) {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return this;
+        }
+        currentNode = currentNode.right;
+      } else {
+        return undefined;
+      }
+    }
+  }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(90);
+tree.insert(75);
+tree.insert(110);
+tree.insert(130);
+tree.insert(60);
+tree.insert(82);
+tree.insert(25);
+tree.insert(50);
+tree.insert(100);
+
+console.log(tree);
+
+// expected output:
+//               90
+//         75         110
+//      60    82  100     130
+//  25
+//     50
