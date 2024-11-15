0123b9ca960931433bd71652d5b1984d3cd0fa35
Nghia Le Minh <wowminhnghia@gmail.com>
2024-11-16 02:45:11 +0700

    implement remove largest node method on max binary heap

--- src/maxBinaryHeap.js
+++ src/maxBinaryHeap.js
@@ -20,6 +20,48 @@ class MaxBinaryHeap {
       currentIndex = parentIndex;
     }
   }
+
+  extractMax() {
+    if (!this.values.length) return undefined;
+    let lastIndex = this.values.length - 1;
+    const removedElement = this.values[0];
+    this.values[0] = this.values[lastIndex];
+    this.values.pop();
+    this.sinkDown();
+    return removedElement;
+  }
+
+  sinkDown() {
+    let currentIndex = 0;
+    const length = this.values.length;
+    const currentElement = this.values[0];
+
+    while (true) {
+      const leftChildIndex = 2 * currentIndex + 1;
+      const rightChildIndex = leftChildIndex + 1;
+      let leftChild, rightChild;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > currentElement) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (!swap && rightChild > currentElement) ||
+          (swap && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (!swap) break;
+      this.values[currentIndex] = this.values[swap];
+      this.values[swap] = currentElement;
+      currentIndex = swap;
+    }
+  }
 }
 
 const heap = new MaxBinaryHeap();
@@ -27,6 +69,7 @@ heap.insert(55);
 heap.insert(1);
 heap.insert(45);
 heap.insert(199);
+heap.insert(20);
 
 console.log(heap);
 
@@ -35,3 +78,14 @@ console.log(heap);
 //               55                       41
 //        39            45        12              33
 //     1      18    27
+
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap);
+
+/** expected output:
+ *                           55
+ *                  45                41
+ *             39        27      12         33
+ *          1     18
+ * */
