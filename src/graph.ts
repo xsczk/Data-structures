f719f12ef029415f8040e9e1ea160640e7254c67
Nghia Le Minh <wowminhnghia@gmail.com>
2024-11-21 23:30:04 +0700

    adding removeVertext method to the graph class constructor

--- src/graph.ts
+++ src/graph.ts
@@ -29,15 +29,34 @@ class Graph {
       this.adjacencyList.get(v2).filter((v) => v !== v1),
     );
   }
+
+  removeVertex(v: string) {
+    const connectionList = this.adjacencyList.get(v);
+    for (const connection of connectionList) {
+      this.removeEdge(v, connection);
+    }
+    this.adjacencyList.delete(v);
+  }
 }
 
 const graph = new Graph();
 graph.addVertex('Hanoi');
 graph.addVertex('Ho Chi Minh');
 graph.addVertex('Da Nang');
+graph.addVertex('Los Angeles');
+graph.addVertex('New York');
+graph.addVertex('London');
 graph.addEdge('Hanoi', 'Da Nang');
 graph.addEdge('Hanoi', 'Ho Chi Minh');
+graph.addEdge('Hanoi', 'London');
+graph.addEdge('Hanoi', 'Los Angeles');
+graph.addEdge('Los Angeles', 'New York');
+graph.addEdge('Los Angeles', 'Ho Chi Minh');
+graph.addEdge('Los Angeles', 'Da Nang');
+graph.addEdge('London', 'Da Nang');
+graph.addEdge('London', 'Ho Chi Minh');
+graph.addEdge('New York', 'Da Nang');
 console.log(graph);
-graph.removeEdge('Hanoi', 'Ho Chi Minh');
-graph.removeEdge('Hanoi', 'Da Nang');
+
+graph.removeVertex('Hanoi');
 console.log(graph);
