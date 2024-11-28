642490d4339e52011ec5aa09a7111a5ba60f7c21
Nghia Le Minh <wowminhnghia@gmail.com>
2024-11-29 00:26:09 +0700

    improve a little bit so that we do not need to have base cases

--- src/dynamic programing/fib-memo.ts
+++ src/dynamic programing/fib-memo.ts
@@ -1,9 +1,16 @@
-function fib(n: number, memo: Map<number, number>): number {
+function fib(
+  n: number,
+  memo = new Map([
+    [1, 1],
+    [2, 1],
+  ]),
+): number {
   if (memo.get(n) !== undefined) return memo.get(n);
-  if (n <= 2) return 1;
   const res = fib(n - 1, memo) + fib(n - 2, memo);
   memo.set(n, res);
   return res;
 }
 
-console.log(fib(6, new Map([])));
+console.log(fib(8));
+
+// 1,1,2,3,5,8,13,21
