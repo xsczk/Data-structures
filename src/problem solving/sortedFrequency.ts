3ab457e17b9f56ff5b91112cbeaff767ca842675
Nghia Le Minh <wowminhnghia@gmail.com>
2024-12-01 01:08:38 +0700

    update sortedFrequency with time complexity of O of logn solution

--- src/problem solving/sortedFrequency.ts
+++ src/problem solving/sortedFrequency.ts
@@ -33,7 +33,29 @@ function sortedFrequency(arr: number[], val: number) {
     return right - left + 1
 }
 
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)) // 4
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)) // 1
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)) // 2
-console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)) // -1
+// Time complexity: O(log n)
+function sortedFrequency_logn(arr: number[], val: number) {
+    function findIdx(arr: number[], val: number, isFindFirst = true) {
+        let left = 0, right = arr.length - 1;
+        let result = -1
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (arr[mid] === val) {
+                result = mid
+                isFindFirst ? right = mid - 1 : left = mid + 1
+            } else if (arr[mid] < val) left = mid + 1
+            else right = mid - 1
+        }
+        return result;
+    }
+
+    const firstValIdx = findIdx(arr, val);
+    const lastValIdx = findIdx(arr, val, false);
+    if (firstValIdx === -1 || lastValIdx === -1) return -1
+    return lastValIdx - firstValIdx + 1
+}
+
+console.log(sortedFrequency_logn([1, 1, 2, 2, 2, 2, 3], 2)) // 4
+console.log(sortedFrequency_logn([1, 1, 2, 2, 2, 2, 3], 3)) // 1
+console.log(sortedFrequency_logn([1, 1, 2, 2, 2, 2, 3], 1)) // 2
+console.log(sortedFrequency_logn([1, 1, 2, 2, 2, 2, 3], 4)) // -1
