183ef9fdcfcf79ec83fa573b146e0becdb3aa4db
Nghia Le Minh <wowminhnghia@gmail.com>
2024-12-18 20:09:19 +0700

    more concise of findLongestSubstring version

--- src/problem solving/findLongestSubstring.ts
+++ src/problem solving/findLongestSubstring.ts
@@ -22,22 +22,39 @@ function findLongestSubstring(str: string): number {
       if (!lookup.has(str[end])) {
          lookup.set(str[end], end);
          if (end === str.length - 1) longest = Math.max(longest, end - start + 1);
-         end++
+         end++;
       } else {
-         longest = Math.max(longest, end - start)
+         longest = Math.max(longest, end - start);
          start = lookup.get(str[end]) + 1;
-         lookup.clear()
+         lookup.clear();
          lookup.set(str[start], start);
-         end = start + 1
+         end = start + 1;
       }
    }
    return longest;
 }
 
-console.log(findLongestSubstring('')) // 0
-console.log(findLongestSubstring('rithmschool')) // 7
-console.log(findLongestSubstring('thisisawesome')) // 6
-console.log(findLongestSubstring('thecatinthehat')) // 7
-console.log(findLongestSubstring('bbbbbb')) // 1
-console.log(findLongestSubstring('longestsubstring')) // 8
-console.log(findLongestSubstring('thisishowwedoit')) // 6
+function findLongestSubstringConcise(str: string): number {
+   if (!str.length) return 0;
+   let longest = 1;
+   let start = 0;
+   let end = 1;
+   let lookup = new Map<string, number>([[str[start], start]]);
+   while (end < str.length) {
+      if (lookup.has(str[end])) start = Math.max(start, lookup.get(str[end]) + 1);
+      longest = Math.max(longest, end - start + 1)
+      lookup.set(str[end], end);
+      end++
+   }
+   return longest;
+}
+
+console.log(findLongestSubstringConcise('')); // 0
+console.log(findLongestSubstringConcise('rithmschool')); // 7
+console.log(findLongestSubstringConcise('thisisawesome')); // 6
+console.log(findLongestSubstringConcise('thecatinthehat')); // 7
+console.log(findLongestSubstringConcise('bbbbbb')); // 1
+console.log(findLongestSubstringConcise('longestsubstring')); // 8
+console.log(findLongestSubstringConcise('thisishowwedoit')); // 6
+console.log(findLongestSubstringConcise('thecatinthehat')); // 7
+
